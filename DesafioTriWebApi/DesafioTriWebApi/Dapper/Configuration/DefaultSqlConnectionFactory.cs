7bd9f505f9a2dacab65584b08e2830a6bad5657f
jamile-design <jamile.honorio@laclaw.com.br>
2022-06-24 16:07:09 -0300

    configuração de conexão do banco de dados

---
+++ DesafioTriWebApi/DesafioTriWebApi/Dapper/Configuration/DefaultSqlConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using DesafioTriWebApi.Dapper.Interfaces;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DesafioTriWebApi.Dapper.Configuration
+{
+    public class DefaultSqlConnectionFactory : IConnectionFactory
+    {
+        public IDbConnection Connection()
+        {
+            return new MySqlConnection("Server=127.0.0.1;Database=northwind;Uid=root;Pwd=123456;");
+        }
+    }
+}

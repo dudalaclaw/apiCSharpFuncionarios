843328b22ca10838776901c3d4aef1b62abfc7af
FelipeLaclaw <99351839+FelipeLaclaw@users.noreply.github.com>
2022-07-01 15:32:37 -0300

    Correção de rota do endpoint Get

--- DesafioTriWebApi/DesafioTriWebApi/Controllers/FuncionarioController.cs
+++ DesafioTriWebApi/DesafioTriWebApi/Controllers/FuncionarioController.cs
@@ -14,8 +14,14 @@ namespace DesafioTriWebApi.Controllers
             _funcionario = funcionario;
         }
 
-        
-        [HttpGet("Get")]
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _funcionario.GetAll();
+            return new JsonResult(result);
+        }
+
+        [HttpGet("Get/{id:int}")]
         public IActionResult Get(int id)
         {
             var result = _funcionario.Get(id);
